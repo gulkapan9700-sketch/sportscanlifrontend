ada95179e932e564640f93ae1c788856fe568c3e
gulkapan9700-sketch <gulkapan9700@gmail.com>
2026-03-31 17:19:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import FootballPage from './pages/FootballPage';
+import BasketballPage from './pages/BasketballPage';
+import TennisPage from './pages/TennisPage';
+import Formula1Page from './pages/Formula1Page';
+import MMAPage from './pages/MMAPage';
+import FixtureDetailPage from './pages/FixtureDetailPage';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/futbol" element={<FootballPage />} />
+          <Route path="/futbol/mac/:id" element={<FixtureDetailPage />} />
+          <Route path="/basketbol" element={<BasketballPage />} />
+          <Route path="/tenis" element={<TennisPage />} />
+          <Route path="/formula1" element={<Formula1Page />} />
+          <Route path="/mma" element={<MMAPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30000,
+      refetchInterval: 60000,
+      retry: 2,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
