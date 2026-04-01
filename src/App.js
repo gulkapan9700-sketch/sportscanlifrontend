c9d6a5668eaa66d5bac20392384b7053196d2730
gulkapan9700-sketch <gulkapan9700@gmail.com>
2026-04-01 11:04:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import FootballPage from './pages/FootballPage';
@@ -11,18 +11,17 @@ import FixtureDetailPage from './pages/FixtureDetailPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/futbol" element={<FootballPage />} />
-          <Route path="/futbol/mac/:id" element={<FixtureDetailPage />} />
-          <Route path="/basketbol" element={<BasketballPage />} />
-          <Route path="/tenis" element={<TennisPage />} />
-          <Route path="/formula1" element={<Formula1Page />} />
-          <Route path="/mma" element={<MMAPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/football" element={<FootballPage />} />
+        <Route path="/futbol/mac/:id" element={<FixtureDetailPage />} />
+        <Route path="/basketbol" element={<BasketballPage />} />
+        <Route path="/tenis" element={<TennisPage />} />
+        <Route path="/formula1" element={<Formula1Page />} />
+        <Route path="/mma" element={<MMAPage />} />
+      </Routes>
+    </Layout>
   );
 }
+``
